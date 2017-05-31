a08d8ee9d064b31d788548a7403068e6848aa89b
jnelson1 <jnelson1@hwemail.com>
2017-05-31 18:55:22 +0200

    Add files via upload

---
+++ AutoLayOut4/AutoLayOut4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayOut4
+//
+//  Created by Jon Nelson1 on 5/30/17.
+//  Copyright © 2017 Jon Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
